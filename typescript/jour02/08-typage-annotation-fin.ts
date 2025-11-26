d3b86bae4756338e49bd09d613d8d122bce9c527
Malik H <malik.h@webdevpro.net>
2025-11-26 12:34:40 +0100

    exo

--- typescript/jour02/08-typage-annotation-fin.ts
+++ typescript/jour02/08-typage-annotation-fin.ts
@@ -23,3 +23,24 @@ const tableau3 : ITblCoordonnees = [
     { x : 1 , y : 2 },
     { x : 2 , y : 3  }
 ]
+
+/**
+ * créer le fichier 09-exo.ts
+ * ce fichier va contenir plusieurs variables
+veuillez les typer correctement :
+
+// liste1 = "pêche poire ananas";
+// liste2 = ["pêche" , "poire", "ananas"];
+// liste3 = ["pêche", 4, "poire", 3, "ananas" , 2];
+ liste4 = {
+    items : ["pêche", 4, "poire", 3, "ananas" , 2],
+    budget : 40 ,
+    unite : "euros"
+};
+liste5 = [
+    {id: 1 , nom : "pêche" , quantite : 4},
+    {id: 2 , nom : "poire" , quantite : 3},
+    {id: 3 , nom : "ananas" , quantite : 2},
+];
+
+*/
