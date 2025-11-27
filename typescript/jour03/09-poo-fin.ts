76653c23940c6ad5ce19e10e27da0ae0d340a783
Malik H <malik.h@webdevpro.net>
2025-11-27 17:45:50 +0100

    exo

--- typescript/jour03/09-poo-fin.ts
+++ typescript/jour03/09-poo-fin.ts
@@ -55,3 +55,26 @@ const e = new E()
 e.calcul()
 e.definir_user()
 e.get_information()
+
+/**
+ * créer le fichier 10-exo.ts
+ * 
+ * créer une class Vehicule 
+ * cette class est une class abstraite qui contient 2 méthodes abstraites
+ * consommation doit retourne un chiffre
+ * marque doit retourner un string 
+ * 
+ * cette class Vehicule est la class parent de deux class concrête 
+ * 
+ * VW 
+ * Renault 
+ * 
+ * 
+ * ces deux class doivent implémenter les méthodes abstraites du parent Vehicule
+ * 
+ * pour la class VW elle a deux propriétés privée prix et nom 
+ * utiliser les setter et getter pour les initialiser
+ * 
+ * pour la class Renaul elle a un constructeur qui va initialiser deux propriétés marque et propriétaire
+ * 
+ */
