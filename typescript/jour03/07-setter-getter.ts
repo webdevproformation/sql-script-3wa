17e7bbb764e821c065076c396c3c54fb55dc9672
Malik H <malik.h@webdevpro.net>
2025-11-27 17:05:17 +0100

    exo

--- typescript/jour03/07-setter-getter.ts
+++ typescript/jour03/07-setter-getter.ts
@@ -15,9 +15,7 @@ class A { // style PHP
 }
 
 class B { // style typescript ou Javascript 
-
     private _prix ?: number 
-    
     public set prix( p : number)
     {
         this._prix = p
@@ -34,3 +32,17 @@ const b = new B()
 
 b.prix = 10 ; // en train d'exécuter le setter
 console.log(b.prix)  // en train d'exécuter le getter de la class 
+
+// cas pratique 
+
+/**
+ * créer le fichier 08-exo.ts
+ * ce fichier contient une class Produit 
+ * cette class ne contient de constructeur mais des setter et des getter pour initialiser ses propriétés
+ * cette class contient 2 propriétés ref et prix 
+ * la référence est obligatoirement un chaine de caractère qui contient au moins 5 lettres 
+ * la prix est obligatoirement un chiffre positif 
+ * mettre en place cette logique dans les setters
+ * si la logique n'est respecter lancer une exception qui donne plus d'informations au développeur sur l'erreur rencontrée 
+ * 
+ */
