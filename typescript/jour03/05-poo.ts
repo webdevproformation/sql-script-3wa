ad5b83429e9b2a8a672ca8ba74c48ad18c57dc5b
Malik H <malik.h@webdevpro.net>
2025-11-27 16:15:59 +0100

    créer des class

---
+++ typescript/jour03/05-poo.ts
@@ -0,0 +1,57 @@
+// class depuis la version 5 de javascript 
+
+
+class A {
+
+    // rappel PHP
+    private a ?: number  ;    // propriété null ou un chiffre 
+    public b  ?: number ;     // propriété null ou un chiffre 
+    protected c ?: number ;   // propriété null ou un chiffre 
+
+    // rappel PHP
+    // attention !!! on ne met pas le mot clé function entre 
+    // la portée de la méthode et le nom de la méthode 
+    public use_param()
+    {
+        this.a = 1; // $this->a en PHP mais en typescript et js this.a
+        this.b = 2 ;
+        this.c = 3 ; 
+    }
+
+    use_param2 (){ // si absence de visibilité par défaut public
+
+    }
+
+}
+
+
+class B {
+    public param_a ;
+    public param_b ;
+    public param_c ;
+
+    public constructor( a : string , b : number , c ?: boolean){
+        this.param_a = a ;
+        this.param_b = b ;
+        if( c ) {
+            this.param_c = c ;
+        }
+    }
+}
+
+// propriété promu
+
+class C {
+    constructor(
+        public a : string , // à la fois créer une propriété dans la class ET l'initialiser 
+        public b : number ,
+        public c : boolean
+    ){}
+}
+
+const c = new C( "a", 1 , true );
+
+// la propriété a est disponible dans l'objet c et a pour valeur "a"
+c.a 
+c.b 
+c.c 
