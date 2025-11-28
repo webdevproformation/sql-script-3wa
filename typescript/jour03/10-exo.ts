ac5e47644e1a70cd3a5f37d942e0b9b7bb30672f
Malik H <malik.h@webdevpro.net>
2025-11-28 10:09:25 +0100

    types utilitaires

--- typescript/jour03/10-exo.ts
+++ typescript/jour03/10-exo.ts
@@ -61,3 +61,4 @@ class Renault extends Vehicule{
         return "";
     }
 }
+
---
+++ typescript/jour04/01-start.ts
@@ -0,0 +1,96 @@
+// fonctions utilitaires 
+
+// ajoute le typage sur javascript 
+
+// créer des types depuis d'autre type 
+
+type Etudiant = string | number ; 
+
+// <https://www.typescriptlang.org/docs/handbook/utility-types.html>
+
+type Formation = {
+    nom : string ,
+    duree : number ,
+    matieres : Array<string>
+}
+
+// créer un nouveau type depuis ce type 
+
+type FormationFacultative = Partial<Formation> // Partial<> est une fonction utilitaire 
+
+/*
+type FormationFacultative = {
+    nom ?: string ,
+    duree ?: number ,
+    matieres ?: Array<string>
+}
+*/
+
+type FormationSansDuree = Omit<Formation , 'duree'> // Omit<> supprimer une propriété et conserver les autres 
+
+/*
+type FormationSansDuree = {
+    nom : string ,
+    matieres : Array<string>
+}
+*/
+
+const a = 1 ; 
+
+class Exo
+{
+    constructor(
+        private readonly duree :  number  // la propriété duree est donner une valeur UNE SEULE FOIS
+    )
+    {
+
+    }
+}
+
+type FormationBloquee = Readonly<Formation> // fonction utilitaire 
+
+/*
+type FormationBloquee = {
+   readonly nom : string ,
+   readonly duree : number ,
+   readonly matieres : Array<string>
+}
+*/
+
+// commande qui contient des produits 
+// chaque produit doit contenir 2 clé id et prix
+
+type ListProduit = 'ps4' | 'ps5';
+type Produit = {  id : number , prix : number }
+
+type Commande = Record<ListProduit , Produit> // très utilisé en typescript 
+                                              // permet de décrire en détail les clés et les valeurs d'un objet 
+
+const commande  : Commande = {
+    ps4 : {id : 1 , prix : 10},
+    ps5 : {id : 2 , prix : 20}
+}
+
+// Parameters / ReturnType 
+
+function requete ( url : string , code_retour : number ) : Array<number | boolean>
+{
+    return [ 1 ];
+}
+
+type ParamFonctionRequete = Parameters<typeof requete> ; 
+
+/**
+ParamFonctionRequete = {
+    url : string 
+    code_retour : number 
+}
+ * 
+ */
+
+type RetourFonctionRequete = ReturnType<typeof requete>
+
+/**
+RetourFonctionRequete = Array<number | boolean>
+ * 
+ */
