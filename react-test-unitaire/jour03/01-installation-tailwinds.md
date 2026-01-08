dce1b50f83a259b5e58525aff5b296bffe482e00
Malik H <malik.h@webdevpro.net>
2026-01-08 15:26:23 +0100

    ajouter tailwind et mis en page composant CreatePoll

---
+++ react-test-unitaire/jour03/01-installation-tailwinds.md
@@ -0,0 +1,5 @@
+<https://tailwindcss.com/docs/installation/using-vite>
+
+```sh
+npm install tailwindcss @tailwindcss/vite
+```
--- react-test-unitaire/pollbuilder/src/components/CreatePoll.tsx
+++ react-test-unitaire/pollbuilder/src/components/CreatePoll.tsx
@@ -6,17 +6,25 @@ export const CreatePoll : React.FC<{}> = () => {
     const hook = useCreatePoll(); 
     return (
         <div>
-            <h1>Création de Questionnaire</h1>
+            <h1 className="text-3xl font-bold underline mb-4">Création de Questionnaire</h1>
             <input 
                 type="text" 
                 value={ hook.form.pollTitle } 
                 placeholder="Entrer le titre du sondage" 
                 onChange={() => {}} 
+                className="bg-gray-100 border border-gray-300 rounded py-2 px-4 focus:outilne-none focus:ring-2 focus:ring-indigo-400"
             />
-            <button
-                disabled={ !hook.isSubmittable }
-                onClick={hook.submitPoll}
-            >Ajouter une question</button>
+            <div className="my-4">
+                <button
+                    disabled={ !hook.isSubmittable }
+                    onClick={hook.submitPoll}
+                    className="
+                        bg-green-500  text-white font-bold py-2 px-2 rounded 
+                        hover:bg-green-700
+                        "
+                >Ajouter une question</button>
+            </div>
+            
         </div>
     )
 }
